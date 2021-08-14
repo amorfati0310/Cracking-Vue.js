83ab198efb769dfe2352923622fa3e150bd94561
amorfati0310 <lyhy0310@gmail.com>
2021-08-14 17:47:33 +0900

    feat: config base 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: 'Cracking Vue.js',
 	description: 'JavaScript, Vue.js, Nuxt.js',
-	base: '/vue-camp/',
+	base: 'https://amorfati0310.github.io//Cracking-Vue.js',
 	head: [
 		['link', { rel: 'icon', href: '/logo.png' }],
 		['link', { rel: 'manifest', href: '/manifest.json' }],
